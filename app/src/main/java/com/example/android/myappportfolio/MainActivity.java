1ce7759d3e3d7e437863d6a2ebfd344f576e74ae
Fabio Nowaki <fabio.nowaki@gmail.com>
2017-02-13 23:07:45 -0200

    Ajusta label e adiciona onClick action

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.android.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] ids = {R.id.btn_build_bigger, R.id.btn_capstone, R.id.btn_go_ubiquitous, R.id.btn_material, R.id.btn_popular_movies, R.id.btn_stock_hawk};
+        for (int id:ids) {
+            Button b = (Button) findViewById(id);
+            b.setOnClickListener(this);
+        }
     }
+
+    public void onClick(View v)
+    {
+        Button b = (Button) findViewById(v.getId());
+        Toast.makeText(MainActivity.this, "This button will launch " + b.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+
 }
